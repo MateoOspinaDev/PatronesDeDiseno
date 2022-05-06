878e9836d40b68e99dfe6f7fcc5baba01af4cbc1
MateoOspinaA <89462921+MateoOspinaA@users.noreply.github.com>
2022-05-06 14:01:10 -0500

    Singleton

---
+++ Creacionales/Singleton/src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        SoyUnico ricardo = SoyUnico.getSingletonInstance("Ricardo Moya");
+        SoyUnico ramon = SoyUnico.getSingletonInstance("Ramón Invarato");
+
+        // ricardo y ramon son referencias a un único objeto de la clase SoyUnico
+        System.out.println(ramon.getNombre());
+        System.out.println(ricardo.getNombre());
+    }
+}
